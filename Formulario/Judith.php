c7c6f3ababb6da5e41d1717428f01d09d8069755
Judith soro Corvo <judithsorocorvo@alumno.ieselrincon.es>
2023-10-03 11:54:27 +0100

    Formulario Judith

---
+++ Formulario/Judith.php
@@ -0,0 +1,79 @@
+<html>
+    <body>
+
+        <form>
+
+        <fieldset>
+    <hr>
+        <label style=> Nombre : </label>
+        <input type="text" value="Introduce tu nombre" id="nombre"/>
+        
+    </hr> 
+
+    
+    <hr>
+        <label for="mail">  Correo electrónico:  </label>
+        <input type="email" value="Introduce tu correo"  id="mail" name="user_mail" />
+    </hr>
+
+
+
+
+    <hr>
+        <label style=> Contraseña : </label>
+        <input type="text" value="Introduce contraseña" id="contraseña"/>
+        
+    </hr> 
+       
+
+
+
+    <hr>
+        <label>  Selecciona un idioma: </label>
+   
+        <select name="idi">
+
+        <option>Inglés</option>
+        <option  >Español</option>
+        <option>Italiano</option>
+        <option>Alemán</option>
+        <option>Francés</option>
+        <option>Chino</option>
+        </select>
+     </hr>
+
+     
+       
+        <hr>
+        
+        <label>  Selecciona los módulos que has cursado </label>
+        <select name="modulo" id="modulo" multiple>
+      
+        <option>1º de Daw</option>
+        <option  >1º de ASIR </option>
+        <option>2º de DAW</option>
+        <option>2º de ASIR</option>
+   
+    
+        </select>
+
+        </hr>
+
+
+
+
+   
+        </fieldset>
+
+        <?php
+        
+        
+        
+        ?>
+
+        </form>
+
+    </body>
+
+
+</html>
